9ba8a2977e52594424f3359998173bae171401ff
Abhilash <bnvsabhi30@gmail.com>
2026-05-31 20:32:06 +0530

    minor update

--- boor-fam/app/api/tree/route.ts
+++ boor-fam/app/api/tree/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const sql = neon(process.env.DATABASE_URL!);
   const data = await sql`
-    SELECT u.id, u.primary_name, u.spouse_name, u.dob, u.city, u.state, u.country, t.parent_id, t."order"
+    SELECT u.id, u.primary_name, u.spouse_name, u.dob, u.city,u.gender, u.state, u.country, t.parent_id, t."order"
     FROM users u 
     INNER JOIN family_tree t ON u.id = t.user_id
     ORDER BY t.parent_id, t."order"
