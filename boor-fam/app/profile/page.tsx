5f36797cc5843dafb118af816d13cf2e27b7c595
Abhilash <bnvsabhi30@gmail.com>
2026-06-28 18:55:22 +0530

    Tambola added

--- boor-fam/app/profile/page.tsx
+++ boor-fam/app/profile/page.tsx
@@ -734,7 +734,7 @@ export default function Home() {
             </div>
           </div>
 
-          {/* <FamilyTambola allUsers={allUsers} /> */}
+          <FamilyTambola allUsers={allUsers} />
         </section>
 
         {/* Member Search */}
